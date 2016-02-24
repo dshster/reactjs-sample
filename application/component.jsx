d320754c48e9e8b8a8ee52256b52f1eaae60fad2
Dmitry Shvalyov <dshster@igma.com>
2016-02-24 19:47:16 +0300

    Webpack-dev-server

--- application/component.jsx
+++ application/component.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default class Hello extends React.Component {
 	render() {
+		let myname = 'dmitry';
+
 		return (
-			<h1>Hello title</h1>
+			<div>
+				<h1>Hello title!</h1>
+				<h2>Hello {myname}!</h2>
+			</div>
 		);
 	}
 }
--- application/index.js
+++ application/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Hello from './component.jsx';
 
 const application = document.getElementById('application');
